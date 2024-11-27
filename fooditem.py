484697f66a727f1bb6992c6dd16119022d823501
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-11-28 00:21:48 +0600

    Add quantity parameter to Fooditem constructor

--- fooditem.py
+++ fooditem.py
@@ -1,5 +1,6 @@
 class Fooditem:
-    def __init__(self, name, price):
+    def __init__(self, name, price,quantity):
         self.name = name
         self.price = price
+        self.quantity=quantity
         
